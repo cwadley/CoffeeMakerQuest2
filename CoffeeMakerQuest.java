4f3cab6e40b616b90d3a1303665db8348db38f37
cwadley <clintwadley@gmail.com>
2016-02-13 10:53:34 -0500

    Added CoffeeMakerQuest.java class

---
+++ CoffeeMakerQuest.java
@@ -0,0 +1,20 @@
+/**
+* CoffeeMakerQuest.java
+*
+* Clinton Wadley
+* cvw5@pitt.edu
+* 2/9/16
+* CS1632
+* Deliverable 2
+*
+* Implementation of the game Coffee Maker Quest, demonstrating the use of unit tests
+* and testing-driven development.
+*/
+
+public class CoffeeMakerQuest
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
